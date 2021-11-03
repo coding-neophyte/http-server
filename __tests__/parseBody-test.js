74aa9e5f416a63ef513e9f67ee5be6ec406e4681
Julius Franklin <jjfranklin88@outlook.com>
2021-11-02 20:03:48 -0400

    parseBody test passing

---
+++ __tests__/parseBody-test.js
@@ -0,0 +1,65 @@
+const parseBody = require('../lib/parseBody.js');
+const EventEmitter = require('events');
+
+describe('parse-body', () => {
+  it('should return null if method is invalid', async () => {
+    expect(await parseBody({ method: 'GET' })).toEqual(null);
+    expect(await parseBody({ method: 'DELETE' })).toEqual(null);
+
+
+  });
+
+  it('throws if content-type is not application/json', async () => {
+
+    const req = {
+      method: 'POST',
+      header: {
+        'content-type': 'text/plain'
+      }
+    };
+    expect.assertions(1);
+    try{
+      await parseBody(req);
+    } catch (err){
+      expect(err).toEqual('invalid content type, content must be application/json');
+    }
+
+  });
+
+  it('returns deserialized body from req emitted events', async () => {
+    const req = new EventEmitter();
+    req.header = { 'content-type': 'application/json' };
+    req.method = 'POST';
+    const reqParse = parseBody(req);
+
+    req.emit('data', `{ "name":`);
+    req.emit('data', ` "john" }`);
+    req.emit('end');
+
+    const body = await reqParse;
+
+    expect(body).toEqual({ name: 'john' });
+
+  });
+
+  it('throws if failure happens in deserialization', async () => {
+    const req = new EventEmitter();
+    req.header = { 'content-type': 'application/json' };
+    req.method = 'POST';
+    const reqParse = parseBody(req);
+
+    req.emit('data', '{"not json"}');
+    req.emit ('end');
+
+
+    expect.assertions(1);
+    try{
+      await reqParse;
+    } catch (err){
+      expect(err).toEqual('invalid json');
+    }
+
+
+  });
+
+});
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const http = require('http');
+const app = require('./lib/app.js');
 
 const port = process.env.PORT || 7890;
 
-const server = http.createServer((app) => {
-
-});
+const server = http.createServer(app);
 
 server.listen(port, () => {
   console.log(`Server running at port http://localhost:${port}`);
